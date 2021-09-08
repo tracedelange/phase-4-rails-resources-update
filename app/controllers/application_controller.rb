6614046961165c5535a5a199a6cdd297fc27a1d1
Trace <tracedelange@me.com>
2021-09-08 16:15:50 -0500

    commit tree ba11734458fa64c87787b49d4db7fbccd62b8113

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+
+    def update
+
+        bird = Bird.find(params[:id])
+        if bird
+            bird.update(bird_params)
+            render json: bird
+        else
+            render json: {error: "Bird Not Found"}, status: :not_found
+        end
+    end
+
+    private
+    def bird_params
+        params.permit(:name, :species, :likes)
+
+    end
+
 end
